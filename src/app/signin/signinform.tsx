4a40762443f65bc4f1700ec495e53084a540e87e
Oussama Aouina <oussamaaouina99@gmail.com>
2024-02-04 11:02:12 +0100

    setting up the signin form ui

---
+++ src/app/signin/signinform.tsx
@@ -0,0 +1,26 @@
+export default function SigninForm(props: { children: React.ReactNode }) {
+    return (
+        <div className="flex w-1/2 max-w-[435px] flex-col flex-wrap content-center justify-center whitespace-nowrap rounded-[30px] bg-neutral-100 bg-opacity-60 px-6 py-6 text-lg font-medium text-red-900 shadow-sm backdrop-blur-[27.5px]">
+            <label>username:</label>
+            <input
+                name="username"
+                type="text"
+                className="mt-2.5 items-start justify-center rounded-xl bg-white py-3.5 pl-3 pr-16 text-zinc-500 shadow-sm"
+                placeholder="username..."
+            />
+            <label className="mt-5">password:</label>
+            <input
+                type="password"
+                name="password"
+                className="mt-2.5 items-start justify-center rounded-xl bg-white py-2.5 pl-3 pr-16 text-zinc-500 shadow-sm"
+                placeholder="password..."
+            />
+            <button
+                type="submit"
+                className="mt-5 justify-center self-center rounded-xl bg-red-500 bg-opacity-70 px-7 py-2.5 font-semibold text-white hover:bg-opacity-90"
+            >
+                OK
+            </button>
+        </div>
+    );
+}
