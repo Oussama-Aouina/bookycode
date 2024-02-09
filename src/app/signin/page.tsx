5a2a8d81f532671bea9d23929acce134175adf84
Oussama Aouina <oussamaaouina99@gmail.com>
2024-02-09 17:02:38 +0100

    fixing the responsivness of the signup form on apple devices

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link';
 import SigninForm from './signinform';
 
 export default function SigninPage() {
     return (
-        <div className="flex w-full flex-col items-center border p-5 ">
+        <div className="flex w-full flex-col items-center  ">
             <SigninForm />
+            <span className="mt-5 text-xs font-semibold text-gray-400">
+                Don't have an account?{' '}
+                <Link
+                    href="/signup"
+                    className="text-red-300 hover:text-red-600"
+                >
+                    Sign up
+                </Link>
+            </span>
         </div>
     );
 }
